c775f9fc28f94b2a3b8468718626032f818587ab
benvallack <ben@phototropic.co.uk>
2025-01-23 14:19:35 +0000

    Add files via upload

---
+++ sketchybar/bar.lua
@@ -0,0 +1,10 @@
+local colors = require("colors")
+
+-- Equivalent to the --bar domain
+sbar.bar({
+  height = 36,
+  margin = 6,
+  y_offset = 6,
+  corner_radius = 9,
+  color = colors.bar.bg,
+})
